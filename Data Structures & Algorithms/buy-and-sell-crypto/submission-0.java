8b67d1827cdaad6020717d9e8d4ef4a440f3a123
roy-ca <royca98@gmail.com>
2026-06-13 01:28:54 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minimumPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] <= minimumPrice) {
+                minimumPrice = prices[i];
+            }
+            int profit = prices[i] - minimumPrice;
+            if(profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+}
