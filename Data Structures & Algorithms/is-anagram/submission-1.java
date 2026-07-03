abd76b1cfac3ce9350dd3f5142927a19a7a014fd
roy-ca <royca98@gmail.com>
2026-07-04 00:41:09 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(int j = 0; j < t.length(); j++) {
+            char ch = t.charAt(j);
+
+            if(!map.containsKey(ch)) {
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if(map.get(ch) == 0) {
+                map.remove(ch);
+            }
+        }
+        return map.isEmpty();
+    }
+}
