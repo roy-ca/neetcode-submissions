005c6ca04ab1515f89dc08753742a02d1d43a779
roy-ca <royca98@gmail.com>
2026-07-20 13:03:09 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> charStack = new Stack<>();
+       for(int i = 0; i < s.length(); i++) {
+        char ch = s.charAt(i);
+
+        if(ch == '(') {
+            charStack.push(')');
+        }
+        else  if(ch == '{') {
+            charStack.push('}');
+        }
+        else if(ch == '['){
+            charStack.push(']');
+        }
+        else {
+            if(charStack.isEmpty() || ch != charStack.pop()) {
+                return false;
+            }
+        }
+       }
+       return charStack.isEmpty();
+    }
+}
+
